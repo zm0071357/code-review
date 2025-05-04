caba658769a4ff8b7af4439f81362e1a1abf00bb
zm0071357 <834302225@qq.com>
2025-05-04 19:39:24 +0800

    feat:DeepSeek代码评审

---
+++ code-review-starter/src/main/java/code/review/sdk/domain/model/PromptTokensDetails.java
@@ -0,0 +1,8 @@
+package code.review.sdk.domain.model;
+
+import lombok.Data;
+
+@Data
+public class PromptTokensDetails {
+    private int cached_tokens;
+}
--- code-review-starter/src/main/java/code/review/sdk/domain/model/Usage.java
+++ code-review-starter/src/main/java/code/review/sdk/domain/model/Usage.java
@@ -1,4 +1,13 @@
 package code.review.sdk.domain.model;
 
+import lombok.Data;
+
+@Data
 public class Usage {
+    private int prompt_tokens;
+    private int completion_tokens;
+    private int total_tokens;
+    private PromptTokensDetails prompt_tokens_details;
+    private int prompt_cache_hit_tokens;
+    private int prompt_cache_miss_tokens;
 }
