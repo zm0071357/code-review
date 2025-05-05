e061b6a40f1dbeb090ccecf41e490c5d863d2edf
zm0071357 <834302225@qq.com>
2025-05-05 19:11:48 +0800

    feat:飞书发送评审通知

--- code-review-starter/src/main/java/code/review/sdk/CodeReviewConfig.java
+++ code-review-starter/src/main/java/code/review/sdk/CodeReviewConfig.java
@@ -11,10 +11,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * 配置入口
@@ -34,22 +31,27 @@ public class CodeReviewConfig {
         Process process = processBuilder.start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
-
         StringBuilder diffCode = new StringBuilder();
         while ((line = reader.readLine()) != null) {
             diffCode.append(line);
         }
-
         int exitCode = process.waitFor();
         System.out.println("Exited with code:" + exitCode);
         System.out.println("评审代码:" + diffCode.toString());
-
+        // 获取分支名
+        String branchName = getBranchName();
         // DeepSeek代码评审
         String log = codeReview(diffCode.toString());
         // 写入日志
         String writeLogUrl = writeLog(token, log);
         System.out.println("日志地址:" + writeLogUrl);
-
+        File currentDirectory = new File(".");
+        // 获取父目录（即.git所在的目录）
+        File parentDirectory = currentDirectory.getAbsoluteFile().getParentFile();
+        // 获取项目名，通常是父目录的名称
+        String projectName = parentDirectory.getName();
+        // 发送通知至飞书
+        pushMessage(projectName, branchName, writeLogUrl);
     }
 
     private static String codeReview(String diffCode) throws IOException {
@@ -126,6 +128,60 @@ public class CodeReviewConfig {
         git.commit().setMessage("Add new File").call();
         git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(token, "")).call();
         return "https://github.com/zm0071357/code-review-log" + "/blob/master/" + dateFolderName + "/" + fileName;
+    }
+
+    private static String getBranchName() throws Exception {
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "rev-parse", "--abbrev-ref", "HEAD");
+        processBuilder.directory(new File("."));
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder branchName = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            branchName.append(line);
+        }
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code: " + exitCode);
+        System.out.println("分支名: " + branchName.toString());
+        return branchName.toString();
+    }
+
+    private static void pushMessage(String projectName, String branchName, String writeLogUrl) throws Exception {
+        String web_hook = "https://open.feishu.cn/open-apis/bot/v2/hook/43bde768-f4d9-4252-8027-d329d2d5c3ea";
+        String msg = "项目：".concat(projectName).concat("，\n")
+                .concat("分支：").concat(branchName).concat("，\n")
+                .concat("代码评审通知：").concat(writeLogUrl);
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("msg_type", "text");
+        JSONObject content = new JSONObject();
+        content.put("text", msg);
+        requestBody.put("content", content);
+        String json = JSON.toJSONString(requestBody);
+
+        URL url = new URL(web_hook);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = json.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder sb = new StringBuilder();
+        while ((inputLine = in.readLine()) != null){
+            sb.append(inputLine);
+        }
+        in.close();
+        connection.disconnect();
+        System.out.println(sb.toString());
     }
 
     private static String generateRandomString(int length) {
